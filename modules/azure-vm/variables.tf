61ef207f39fae9a2d7c0583c702d7284f31665d1
fazdamoa <fraserdavidson@gmail.com>
2020-06-18 12:12:09 +0100

    Update module var values

--- modules/azure-vm/variables.tf
+++ modules/azure-vm/variables.tf
@@ -5,7 +5,16 @@ variable "subnet_name" {}
 variable "rg_name" {}    
 variable "app_name" {}
 variable "admin_password" { default = "Password123!" }
-variable "vm_size" { default = "Standard_NC6_Promo", description = "Default value is GPU promo."}
-variable "apps_to_install" { type = "string", description = "Must be: PPro19" }
+variable "vm_size" {
+    default = "Standard_NC6_Promo"
+    description = "Default value is GPU promo." 
+}
+variable "apps_to_install" { 
+    type = "string"
+    description = "Must be: PPro19"
+}
 variable "shutdown_timezone" { default = "GMT Standard Time" }
-variable "shutdown_time" { description = "Time in 24 hour time", default = "2200" }
+variable "shutdown_time" { 
+    description = "Time in 24 hour time"
+    default = "2200" 
+}
