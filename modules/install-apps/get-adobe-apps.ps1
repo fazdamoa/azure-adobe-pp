a5dfc3f3644b33e3100bb6d85e7fde1965e82dd7
fazdamoa <fraserdavidson@gmail.com>
2020-06-19 10:47:44 +0100

    Add photoshop, ppro20, aftereffects

--- modules/install-apps/get-adobe-apps.ps1
+++ modules/install-apps/get-adobe-apps.ps1
@@ -1,5 +1,8 @@
 # List Magnet Links for apps
 $PPro19 = "magnet:?xt=urn:btih:ea575decbaa3934db12e52d5ce8cb9efac7f4796"
+$PPro20 = "magnet:?xt=urn:btih:383cddcecc07c8868dc200f3d8f6a3b3c983824b"
+$AEffects20 = "magnet:?xt=urn:btih:3f626db1c34dbfb807a46f5330747c2bb99f57a6"
+$PShop20 = "magnet:?xt=urn:btih:53b95d5c10d66a9a0b16b51b1865c5bcebcab678"
 
 # Create Temp directory to download ISO
 New-Item -ItemType Directory -Force -Path c:\temp
@@ -11,4 +14,16 @@ Invoke-WebRequest -Uri https://raw.githubusercontent.com/fazdamoa/azure-adobe-pp
 # Get the installer ISOs
 if ($args.contains("PPro19")) {
     Start-Process -FilePath c:\temp\torrent.exe -Wait -NoNewWindow -ArgumentList "download",$PPro19
+}
+
+if ($args.contains("PPro20")) {
+    Start-Process -FilePath c:\temp\torrent.exe -Wait -NoNewWindow -ArgumentList "download",$PPro20
+}
+
+if ($args.contains("AEffects20")) {
+    Start-Process -FilePath c:\temp\torrent.exe -Wait -NoNewWindow -ArgumentList "download",$AEffects20
+}
+
+if ($args.contains("Pshop20")) {
+    Start-Process -FilePath c:\temp\torrent.exe -Wait -NoNewWindow -ArgumentList "download",$Pshop20
 }
--- terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "location" {}
-variable "vnet_name" {}
-variable "vnet_rg"  {}   
-variable "subnet_name" {}
-variable "rg_name" {}    
-variable "app_name" {}
-variable "vm_size" { default = "Standard_NC6_Promo", description = "Default value is GPU promo."}
-variable "admin_password" { default = "Password123!" }
-variable "apps_to_install" { type = "string", description = "Must be: PPro19" }
