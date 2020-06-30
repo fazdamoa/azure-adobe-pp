8780132eba9beb9376eb5a5fdb67a67b6f4f1b44
fazdamoa <fraserdavidson@gmail.com>
2020-06-30 16:41:58 +0100

    ugj

--- files/test.sh
+++ files/test.sh
@@ -3,5 +3,5 @@ set -e
 terraform fmt -write=false ./examples/azure-vm-with-ppro
 cd ./test
 go get -d
-go mod init github.com/fazdamoa/azure-adobe-pp
+#go mod init github.com/fazdamoa/azure-adobe-pp
 go test -v -timeout 30m
